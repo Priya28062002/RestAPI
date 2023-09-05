7a35b4e5525f0233c0ef6f90706aed325f4d957e
Priya28062002 <108514514+Priya28062002@users.noreply.github.com>
2023-09-05 09:59:10 +0530

    Delete vehicleapp directory

--- vehicleapp/src/main/java/com/priya/vehicleapp/repository/ChargesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.priya.vehicleapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.priya.vehicleapp.model.Charges;
-
-public interface ChargesRepository extends JpaRepository<Charges, Integer> {
-
-}
