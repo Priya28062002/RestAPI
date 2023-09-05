1ba8f6a86993ee2a622edcf5260918011c36c938
Priya28062002 <108514514+Priya28062002@users.noreply.github.com>
2023-09-05 10:07:24 +0530

    Delete vehicleservice directory

--- vehicleservice/src/main/java/com/springapp/vehicleservice/repository/ChargesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springapp.vehicleservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springapp.vehicleservice.model.Charges;
-
-public interface ChargesRepository extends JpaRepository<Charges, Integer> {
-
-}
--- vehicleservice/src/main/java/com/springapp/vehicleservice/service/ValidationService.java
+++
@@ -1,7 +0,0 @@
-package com.springapp.vehicleservice.service;
-
-public interface ValidationService {
-
-    public boolean isValidRequest(String emailId, String password, String name, String contactNo);
-    
-}
